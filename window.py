4fe0481529ef6f1f7361f30397debac59202824f
Bulat <bulat20031006@gmail.com>
2022-09-17 19:26:48 +0300

    Добавил кнопку

--- window.py
+++ window.py
@@ -1,7 +1,15 @@
+class Button:
+    def __init__(self, name: str):
+        self._name = name
+
+    def __str__(self):
+        return f"<Button {self._name}>"
+
+
 class Window:
 
-    def __init__(self):
-        pass
+    def __init__(self, buttons: list[Button]):
+        self._buttons = buttons
 
     def show(self):
-        print("Window")
+        print(f"Window; buttons: {', '.join([str(button) for button in self._buttons])}")
